69b36f4ce84e3fb588303ba6d4a8e417754cc9ce
xxx <xxx>
2020-03-26 15:37:33 +0800

    delete jar

--- src/test/java/TestJunit.java
+++ src/test/java/TestJunit.java
@@ -1,17 +1,19 @@
-//import lab.bue.MyProvider;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import lab.bue.MyProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-//@SpringBootTest(classes = lab.Main.class)
-//@RunWith(SpringJUnit4ClassRunner.class)
+import javax.annotation.Resource;
+
+@SpringBootTest(classes = lab.Main.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class TestJunit {
-//    @Resource
-//    private MyProvider provider;
+    @Resource
+    private MyProvider provider;
 
-//    @Test
-//    public void send() {
-//        provider.sendMsg();
-//    }
+    @Test
+    public void send() {
+        provider.sendMsg();
+    }
 }
