a0cc474beda3f28d8c0b4110e5344a6841ac883a
xxx <xxx>
2020-04-11 15:23:19 +0800

    update all guild.gradle

--- Consumer/src/main/java/xx/CMain.java
+++ Consumer/src/main/java/xx/CMain.java
@@ -2,9 +2,11 @@ package xx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+//@ComponentScan("xx.bue")
 @EnableJms
 public class CMain {
     public static void main(String[] args) {
--- Consumer/src/main/java/xx/bue/MyConsumer.java
+++ Consumer/src/main/java/xx/bue/MyConsumer.java
@@ -5,14 +5,15 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.TextMessage;
 
 @Component
 public class MyConsumer {
     @JmsListener(destination = "myqueue")
 //    @SendTo
-    public void takeMsg(TextMessage textMessage) throws JMSException {
-        System.out.println("進入 sendMsg1=" + textMessage.getText());
+    public void takeMsg(TextMessage msg) throws JMSException {
+        System.out.println("進入 sendMsg1=" + msg);
 //        return "xxxxxxxxxx";
     }
 
